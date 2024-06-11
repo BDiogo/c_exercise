1813cd6f0696108f90d019d0c280a9699e2dc500
Beatriz Diogo <beanes.diogo@gmail.com>
2024-06-11 22:51:37 +0100

    404 page

--- src/PageNotFound.vue
+++ src/PageNotFound.vue
@@ -1,8 +1,28 @@
 <template>
-  <div class="row row-not-found">
-    <div class="col text-box">
-      <h1 class="super-big">404</h1>
-      <h1>Page Not Found</h1>
-    </div>
-  </div>
+  <v-container class="page-not-found">
+    <div class="text-emoji">🚨</div>
+    <div class="text-h1">404</div>
+    <div class="text-h4">Page Not Found</div>
+  </v-container>
 </template>
+
+<style scoped lang="scss">
+.page-not-found {
+  height: 100vh;
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  height: 90%;
+  
+  .text-emoji {
+    line-height: 1.2em;
+    font-size: 12rem;
+  }
+  div {
+    display: block;
+    margin: 0.5rem;
+    font-weight: 600;
+  }
+}
+</style>
